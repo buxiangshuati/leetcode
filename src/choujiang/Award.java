c74f321cca24ff876e6fa3c55b90ca36f088d4c4
Qian <Qian@qq.com>
2025-08-10 11:40:48 +0800

    master COMMIT

--- src/choujiang/Award.java
+++ src/choujiang/Award.java
@@ -5,11 +5,11 @@ public class Award {
         LotterySystem system = new LotterySystem(new LotteryImpl());
         system.startDraw("用户A");
 
-        system.setStrategy(new FixedLottery());
+
         system.startDraw("用户B");
 
         system.startDraw("用户C");
-
+        system.setStrategy(new FixedLottery());
         system.startDraw("用户D");
 
         system.startDraw("用户E");
